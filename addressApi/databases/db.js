2c0ac9ca77932992191edfb7202b76f4c2681c29
badung7576x <Dangbadung7576@gmail.com>
2020-12-08 20:51:23 +0700

    Init code

---
+++ addressApi/databases/db.js
@@ -0,0 +1,11 @@
+'use strict';
+const mysql = require('mysql');
+
+const DB = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "province"
+});
+
+module.exports = DB
