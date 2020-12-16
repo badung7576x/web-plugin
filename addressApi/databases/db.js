669c4d2f4f0515ea8a505beab436ea21ecc6c983
bacht <bachgtung144007@gmail.com>
2020-12-16 16:32:48 +0700

    clear cache option

--- addressApi/databases/db.js
+++ addressApi/databases/db.js
@@ -5,7 +5,7 @@ const DB = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "province"
+  database: process.env.DB_NAME || "city"
 });
 
 module.exports = DB
